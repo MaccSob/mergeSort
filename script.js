b128802593d194d671fbdd694824dbf5b23388cc
MaccSob <71sobmaciej@gmail.com>
2024-05-27 23:36:05 +0200

    added splicing arrays, will finish tomorrow

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+function mergeSort(left, right) {
+    let start = [1,2,3,4,5,6];
+    let mid = Math.ceil(start.length / 2);     
+    let end = [];
+     left = start.slice(0, mid)
+ right = start.slice(mid)
+ console.log(left);
+ console.log(right);
+ console.log(end);
 
+ }
 
-function mergeSort(arr) {
-    arr = [];
-
- 
-
-}
+mergeSort()
